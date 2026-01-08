f3e637cafaec47db366bf52ee9543bfb05117966
V1629 <vaibhavmishraad794@gmail.com>
2026-01-09 01:51:58 +0530

    added serializer.py and created a post model

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,8 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from .models import Post
+from .serializers import PostSerializer
 
 
 ###a MOCK POSTS DATABSE
