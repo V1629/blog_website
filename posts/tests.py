f0b3d97f7cf2d286c3c189cbaa9c34112e026efb
V1629 <vaibhavmishraad794@gmail.com>
2026-01-20 18:54:47 +0530

    feat : Tests - done tests of api's using unit testing - used restframework's APITestCase and APIRequestFactory - these tests was written for homepage, attaching a sample user and creating a sample posts for that - later going to change the code for playing around, thats why commited this to store on cloud

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,59 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase,APIRequestFactory
+from django.urls import reverse
+from rest_framework import status
+from .views import PostListCreateView
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class HelloWorldTestCase(APITestCase):
+    def test_hello_world(self):
+        response = self.client.get(reverse('posts_home'))
+
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.data["message"],"Hello world")
+
+
+class PostListCreateTestCase(APITestCase):
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = PostListCreateView.as_view()
+        self.url = reverse('list_posts')
+        self.user = User.objects.create(
+            username = "ig_vaibhav_mishra",
+            email = "vaibhavmishraad79233@gmail.com",
+            password = "password#123"
+        )
+
+    def test_list_posts(self):
+        
+        request = self.factory.get(self.url)
+        response = self.view(request)
+
+        print(response.data)
+
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.data['count'],0)
+        self.assertEqual(response.data['results'],[])
+
+
+    def test_post_creation(self):
+        sample_post = {
+            "title" : "Sample post",
+            "content" : "this is a sample post for unit testing"
+        }
+        request = self.factory.post(self.url,sample_post)
+
+        request.user = self.user
+
+        response = self.view(request)
+
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+
+
+    
+
 
 # Create your tests here.
